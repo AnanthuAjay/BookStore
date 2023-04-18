d3c386f0b8ad8e09bb5e0fbae02ff0a957fc598e
Ananthu <Ananthu@LAPTOP-PVIQ2SE4>
2023-04-19 00:42:33 +0530

    cart and user service layer test over

--- BookstoreAPI/src/test/java/com/example/bookstore/service/CartServiceTest.java
+++ BookstoreAPI/src/test/java/com/example/bookstore/service/CartServiceTest.java
@@ -1,5 +1,120 @@
 package com.example.bookstore.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.example.bookstore.exception.NotFoundException;
+import com.example.bookstore.model.Book;
+import com.example.bookstore.model.Cart;
+import com.example.bookstore.model.CartItem;
+import com.example.bookstore.model.User;
+import com.example.bookstore.repository.BookRepository;
+import com.example.bookstore.repository.CartRepository;
+
+@SpringBootTest
 public class CartServiceTest {
 
+    private CartService cartService;
+
+    @MockBean
+    private CartRepository cartRepository;
+
+    @MockBean
+    private BookRepository bookRepository;
+
+    
+
+    @Test
+    public void testAddItemToCart() {
+        Long bookId = 1L;
+        Integer quantity = 2;
+
+        Book book = new Book();
+        book.setId(bookId);
+        book.setTitle("Test Book");
+        book.setPrice(10.0);
+
+        CartItem cartItem = new CartItem();
+        cartItem.setId(1L);
+        cartItem.setQuantity(quantity);
+
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setUser(new User());
+
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(cartRepository.findById(cartItem.getId())).thenReturn(Optional.of(cart));
+        when(cartRepository.save(cart)).thenReturn(cart);
+
+        cartService.addItemToCart(bookId, cartItem);
+
+        assertEquals(1, cart.getCartItems().size());
+        assertNotNull(cart.getCartItems().get(0));
+        assertEquals(book, cart.getCartItems().get(0).getBook());
+        assertEquals(quantity, cart.getCartItems().get(0).getQuantity());
+    }
+
+    @Test
+    public void testGetCartItemsByUserId() {
+        Long userId = 1L;
+
+        Cart cart = new Cart();
+        cart.setId(userId);
+        cart.setUser(new User());
+
+        List<CartItem> cartItems = new ArrayList<>();
+        cartItems.add(new CartItem());
+        cartItems.add(new CartItem());
+        cartItems.add(new CartItem());
+
+        cart.setCartItems(cartItems);
+
+        when(cartRepository.findById(userId)).thenReturn(Optional.of(cart));
+
+        List<CartItem> result = cartService.getCartItemsByUserId(userId);
+
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    public void testUpdateCartItemQuantity() {
+        Long cartItemId = 1L;
+        Integer newQuantity = 3;
+
+        CartItem cartItem = new CartItem();
+        cartItem.setId(1L);
+        cartItem.setQuantity(newQuantity);
+
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setUser(new User());
+
+        List<CartItem> cartItems = new ArrayList<>();
+        CartItem item = new CartItem();
+        item.setId(cartItemId);
+        item.setQuantity(2);
+        item.setBook(new Book());
+        cartItems.add(item);
+
+        cart.setCartItems(cartItems);
+
+        when(cartRepository.findById(cartItem.getId())).thenReturn(Optional.of(cart));
+        when(cartRepository.save(cart)).thenReturn(cart);
+
+        CartItem result = cartService.updateCartItemQuantity(cartItemId, cartItem);
+
+        assertEquals(cartItemId, result.getId());
+        assertEquals(newQuantity, result.getQuantity());
+    }
 }
--- BookstoreAPI/src/test/java/com/example/bookstore/service/UserServiceTest.java
+++ BookstoreAPI/src/test/java/com/example/bookstore/service/UserServiceTest.java
@@ -1,5 +1,147 @@
 package com.example.bookstore.service;
 
-public class UserServiceTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.example.bookstore.exception.NotFoundException;
+import com.example.bookstore.model.User;
+import com.example.bookstore.repository.UserRepository;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+	
+	@Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void testFindByEmail() {
+        User user = new User();
+        user.setEmail("test@example.com");
+
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
+
+        Optional<User> foundUser = userService.findByEmail("test@example.com");
+        assertTrue(foundUser.isPresent());
+        assertEquals(user.getEmail(), foundUser.get().getEmail());
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setName("John Doe");
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        User savedUser = userService.save(user);
+        assertNotNull(savedUser);
+        assertEquals(user.getName(), savedUser.getName());
+    }
+
+    @Test
+    public void testDeleteById() {
+        Long id = 1L;
+
+        userService.deleteById(id);
+        verify(userRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setName("John Doe");
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        User savedUser = userService.createUser(user);
+        assertNotNull(savedUser);
+        assertEquals(user.getName(), savedUser.getName());
+    }
+
+    @Test
+    public void testGetUserById() {
+        Long id = 1L;
+
+        User user = new User();
+        user.setId(id);
+        user.setName("John Doe");
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+
+        User foundUser = userService.getUserById(id);
+        assertNotNull(foundUser);
+        assertEquals(id, foundUser.getId());
+        assertEquals(user.getName(), foundUser.getName());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        Long id = 1L;
+
+        User user = new User();
+        user.setId(id);
+        user.setName("John Doe");
+
+        User updatedUser = new User();
+        updatedUser.setId(id);
+        updatedUser.setName("Jane Doe");
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        when(userRepository.save(user)).thenReturn(updatedUser);
+
+        User savedUser = userService.updateUser(id, updatedUser);
+        assertNotNull(savedUser);
+        assertEquals(id, savedUser.getId());
+        assertEquals(updatedUser.getName(), savedUser.getName());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testUpdateUserNotFound() {
+        Long id = 1L;
+
+        User user = new User();
+        user.setId(id);
+        user.setName("John Doe");
+
+        User updatedUser = new User();
+        updatedUser.setId(id);
+        updatedUser.setName("Jane Doe");
+
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        userService.updateUser(id, updatedUser);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        Long id = 1L;
+
+        userService.deleteUser(id);
+        verify(userRepository, times(1)).deleteById(id);
+    }
+
+    
+    
 }
